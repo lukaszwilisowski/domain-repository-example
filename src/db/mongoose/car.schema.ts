40e3ef878180dcb6b3a34cede24fd6b9f9b16cd5
Lukasz Wilisowski <l.wilisowski@optimumpareto.com>
2023-01-06 13:17:23 +0100

    examples working

---
+++ src/db/mongoose/car.schema.ts
@@ -0,0 +1,31 @@
+import { Mapping, mapToMongoObjectId } from 'domain-repository';
+import { Schema } from 'mongoose';
+import { ICarAttached } from '../../domain/models/car.model';
+import { ICarMongoEntity } from './car.entity';
+
+export const CarSchema = new Schema<ICarMongoEntity>({
+  name: {
+    type: String,
+    required: true
+  },
+  best_of_all: {
+    type: Boolean,
+    required: true
+  },
+  yearOfProduction: {
+    type: Number,
+    required: true
+  },
+  sold: {
+    type: Date,
+    required: false
+  }
+});
+
+export const mongoCarMapping: Mapping<ICarAttached, ICarMongoEntity> = {
+  id: mapToMongoObjectId,
+  name: 'name',
+  best: 'best_of_all',
+  yearOfProduction: 'yearOfProduction',
+  sold: 'sold'
+};
