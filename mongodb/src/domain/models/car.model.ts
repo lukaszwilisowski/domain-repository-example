d5c60437d7a136e403568970bf34842430a65c89
Lukasz Wilisowski <l.wilisowski@optimumpareto.com>
2023-01-10 18:49:29 +0100

    added comments to models

--- mongodb/src/domain/models/car.model.ts
+++ mongodb/src/domain/models/car.model.ts
@@ -1,7 +1,14 @@
 export type ICar = {
+  /* Mutable name of the car. */
   name: string;
+
+  /* Mutable flag. Equals true for the single, higest ranked car in the system. */
   best: boolean;
+
+  /* Readonly year of production. */
   readonly yearOfProduction: number;
+
+  /* Mutable sale date. Optional means the car was not sold yet. */
   sold?: Date;
 };
 
